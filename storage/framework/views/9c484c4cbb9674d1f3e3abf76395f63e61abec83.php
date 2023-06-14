48a049488f2a1640fb4968a3ea18a8e0d2ece3b2
EvgeniKuznetsy <locrin13@gmail.com>
2023-06-14 10:30:46 +0500

    problem with storage

--- storage/framework/views/9c484c4cbb9674d1f3e3abf76395f63e61abec83.php
+++ storage/framework/views/9c484c4cbb9674d1f3e3abf76395f63e61abec83.php
@@ -10,23 +10,23 @@
     <link rel="stylesheet"
           href="<?php echo e(asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback')); ?>">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php echo e(asset('plugins/fontawesome-free/css/all.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/fontawesome-free/css/all.min.css')); ?>">
     <!-- Ionicons -->
     <link rel="stylesheet" href="<?php echo e(asset('https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css')); ?>">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="<?php echo e(asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')); ?>">
     <!-- iCheck -->
-    <link rel="stylesheet" href="<?php echo e(asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/icheck-bootstrap/icheck-bootstrap.min.css')); ?>">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="<?php echo e(asset('plugins/jqvmap/jqvmap.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/jqvmap/jqvmap.min.css')); ?>">
     <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo e(asset('dist/css/adminlte.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/dist/css/adminlte.min.css')); ?>">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="<?php echo e(asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')); ?>">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="<?php echo e(asset('plugins/daterangepicker/daterangepicker.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/daterangepicker/daterangepicker.css')); ?>">
     <!-- summernote -->
-    <link rel="stylesheet" href="<?php echo e(asset('plugins/summernote/summernote-bs4.min.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/summernote/summernote-bs4.min.css')); ?>">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
@@ -104,9 +104,9 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="<?php echo e(asset('plugins/jquery/jquery.min.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/jquery/jquery.min.js')); ?>"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="<?php echo e(asset('plugins/jquery-ui/jquery-ui.min.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/jquery-ui/jquery-ui.min.js')); ?>"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
     $.widget.bridge('uibutton', $.ui.button)
@@ -114,31 +114,31 @@
 <script src="public/js/buttonJs.js"></script>
 <!-- Bootstrap 4 -->
 
-<script src="<?php echo e(asset('plugins/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/bootstrap/js/bootstrap.bundle.min.js')); ?>"></script>
 <!-- ChartJS -->
-<script src="<?php echo e(asset('plugins/chart.js/Chart.min.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/chart.js/Chart.min.js')); ?>"></script>
 <!-- Sparkline -->
-<script src="<?php echo e(asset('plugins/sparklines/sparkline.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/sparklines/sparkline.js')); ?>"></script>
 <!-- JQVMap -->
-<script src="<?php echo e(asset('plugins/jqvmap/jquery.vmap.min.js')); ?>"></script>
-<script src="<?php echo e(asset('plugins/jqvmap/maps/jquery.vmap.usa.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/jqvmap/jquery.vmap.min.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/jqvmap/maps/jquery.vmap.usa.js')); ?>"></script>
 <!-- jQuery Knob Chart -->
-<script src="<?php echo e(asset('plugins/jquery-knob/jquery.knob.min.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/jquery-knob/jquery.knob.min.js')); ?>"></script>
 <!-- daterangepicker -->
-<script src="<?php echo e(asset('plugins/moment/moment.min.js')); ?>"></script>
-<script src="<?php echo e(asset('plugins/daterangepicker/daterangepicker.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/moment/moment.min.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/daterangepicker/daterangepicker.js')); ?>"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="<?php echo e(asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')); ?>"></script>
 <!-- Summernote -->
-<script src="<?php echo e(asset('plugins/summernote/summernote-bs4.min.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/summernote/summernote-bs4.min.js')); ?>"></script>
 <!-- overlayScrollbars -->
-<script src="<?php echo e(asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')); ?>"></script>
 <!-- AdminLTE App -->
-<script src="<?php echo e(asset('dist/js/adminlte.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/dist/js/adminlte.js')); ?>"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="<?php echo e(asset('dist/js/demo.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/dist/js/demo.js')); ?>"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo e(asset('dist/js/pages/dashboard.js')); ?>"></script>
+<script src="<?php echo e(asset('https://bcfc-31-135-250-34.ngrok-free.app/dist/js/pages/dashboard.js')); ?>"></script>
 </body>
 </html>
 <?php /**PATH C:\OpenServer\domains\kindergarten-master\resources\views/layouts/admin.blade.php ENDPATH**/ ?>
